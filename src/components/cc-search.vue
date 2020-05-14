6dafee4fe3c5bbd8afaf7a4099c609c3ce866e40
pmcp <maartenlauwaert@gmail.com>
2020-05-14 17:10:43 +0200

    forgot downloaded image in graphql

--- src/components/cc-search.vue
+++ src/components/cc-search.vue
@@ -43,7 +43,7 @@
               style="position:relative"
             >
               
-            <g-image v-if="result.item.imageDownloaded" :src="result.item.imageDownloaded" width="400" height="300" style="position:absolute;height: 100%;width: 100%;object-fit: cover;"/>
+            <g-image v-if="result.item.imageDownloaded" :src="result.item.imageDownloaded" width="400" height="100" style="position:absolute;height: 100%;width: 100%;object-fit: cover;"/>
             <g-image v-else-if="result.item.image" :src="result.item.image" width="400" height="300" style="position:absolute;height: 100%;width: 100%;object-fit: cover;"/>
             <!-- TODO: add placeholder image -->
             <div v-else style="position:absolute;height: 100%;width: 100%;object-fit: cover;"></div>
@@ -88,6 +88,7 @@ query Search{
         descr,
         recordedAt,
         image,
+        imageDownloaded,
         text,
         genre,
         audience,
