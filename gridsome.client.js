e1074223c9543a9a2010279958111e4a3e91e6ff
pmcp <maartenlauwaert@gmail.com>
2020-05-18 17:38:19 +0200

    removed unnecssary console log

--- gridsome.client.js
+++ gridsome.client.js
@@ -1,13 +1,11 @@
 export default function (Vue, { router }) {
   if(process.isClient) {
-
-    console.log('here')
     const { default: VueGtm } = require('vue-gtm')
     Vue.use(VueGtm, {
       vueRouter: router,
       id: 'GTM-5TGPDTT',
       enabled: true,
-      debug: true,
+      debug: true
     })
   }
 }
